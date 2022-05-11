7b90e82648d79dde3b26e5ce62c9c4c9bb1cb009
135Marc <a79097@alunos.uminho.pt>
2022-05-11 17:38:08 +0000

    Styling and Templating Structure Added

--- hello.py
+++ hello.py
@@ -8,3 +8,7 @@ bs = Bootstrap5(app)
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+@app.route('/about')
+def about_page():
+    return render_template('about.html')
