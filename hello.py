d30145116d48a2fce4c45900f1de88069410dd16
135Marc <a79097@alunos.uminho.pt>
2022-05-16 17:31:14 +0000

    Forms and FrontEnd Tweaks

--- hello.py
+++ hello.py
@@ -1,9 +1,17 @@
 from ensurepip import bootstrap
 from flask import Flask,render_template
 from flask_bootstrap import Bootstrap5
+from flask_wtf import FlaskForm
+from wtforms.fields import FileField,SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bs = Bootstrap5(app)
+app.config['SECRET_KEY']= '#sEcReTkEy!2220'
+
+class FileForm(FlaskForm):
+    name = FileField('Select a File to Upload',validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 @app.route('/')
 def hello_world():
@@ -12,3 +20,9 @@ def hello_world():
 @app.route('/about')
 def about_page():
     return render_template('about.html')
+
+@app.route('/analyzer',methods=['GET','POST'])
+def analyzer_page():
+    name = None
+    form = FileForm()
+    return render_template('analyzer.html',form=form,name=name)
