3675807cbf6730b0cfed5fbffc92a4ba5176db48
135Marc <a79097@alunos.uminho.pt>
2022-05-27 17:06:10 +0000

    Added Upload Form

--- hello.py
+++ hello.py
@@ -1,16 +1,20 @@
+from dataclasses import dataclass
 from ensurepip import bootstrap
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
+import werkzeug
 from wtforms.fields import FileField,SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bs = Bootstrap5(app)
 app.config['SECRET_KEY']= '#sEcReTkEy!2220'
+app.config['UPLOAD_FOLDER'] = './'
+app.config['MAX_CONTENT_PATH'] = '20'
 
 class FileForm(FlaskForm):
-    name = FileField('Select a File to Upload',validators=[DataRequired()])
+    name = FileField(name='file',label='Select a File to Upload',validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 @app.route('/')
@@ -25,4 +29,7 @@ def about_page():
 def analyzer_page():
     name = None
     form = FileForm()
+    if (request.method == 'POST') :
+        f = request.files['file']
+        f.save(f.filename)
     return render_template('analyzer.html',form=form,name=name)
